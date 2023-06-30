6e062d6585ae3cb285ad5e37babdaae7007322db
fmaring04 <f.maring04@gmail.com>
2023-06-30 18:47:25 -0400

    o

--- newpage/apps.py
+++ newpage/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HuellitasConfig(AppConfig):
+class newPageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'newPage'
--- newpage/urls.py
+++ newpage/urls.py
@@ -1,6 +1,6 @@
 #from django.conf.urls import url
 from django.urls import path
-from  . import views
+from . import views
 
 urlpatterns = [
     path('homepage', views.homepage, name='homepage'),
