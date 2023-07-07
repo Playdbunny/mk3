0c1e6e44aefa375b9b928728d641e7ac3aa430c0
fmaring04 <127951763+fmaring04@users.noreply.github.com>
2023-07-07 11:23:18 -0400

    Update urls.py

--- newpage/urls.py
+++ newpage/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path("homepage/", views.homepage, name="homepage"),
     path("bandanas/", views.bandanas, name="bandanas"),
+    path("correas/", views.correas, name="correas"),
+    path("identificaciones/", views.identificaciones, name="identificaciones"),
+    path("about/", views.about, name="about"),
     path('crud', views.crud, name='crud'),
 ]
