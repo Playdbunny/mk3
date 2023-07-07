4eef660a7d952b21de90db86ae64a57ea9e522ae
fmaring04 <f.maring04@gmail.com>
2023-07-07 04:54:43 -0400

    Update views.py

--- newpage/views.py
+++ newpage/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from django.template import Template, Context
+from django.http import HttpResponse
 
 from .models import Usuario, tipoUsuario
 # Create your views here.
 
 def homepage(request):
-    context = {}
-    return render(request, "templates/homepage.html", context)
+    index = open("D:/Fernanda/Documents/mk3/newPage/templates/homepage.html")
+    template = Template(index.read())
+    index.close()
+    context = Context()
+    document = template.render(context)
+    return HttpResponse(document)
 
