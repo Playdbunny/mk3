8d3e606ba17d6eb4b33c21cf8a1d6379a658eff7
fmaring04 <f.maring04@gmail.com>
2023-07-06 20:23:39 -0400

    0

--- newpage/views.py
+++ newpage/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
+from .models import Usuario, tipoUsuario
 # Create your views here.
 
-
+def homepage(request):
+    usuario = Usuario.objects.all()
+    context = {"usuario": usuario}
+    return render(request, "pages/homepage.html", context)
 
