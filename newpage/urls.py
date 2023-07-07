cba558fc785061b1130f3742cd32121d02b88a48
fmaring04 <f.maring04@gmail.com>
2023-07-07 05:56:36 -0400

    1

--- newpage/urls.py
+++ newpage/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("homepage/", views.homepage, name="homepage"),
+    path("bandanas/", views.bandanas, name="bandanas"),
+    path('crud', views.crud, name='crud'),
 ]
--- newpage/views.py
+++ newpage/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.template import Template, Context
 from django.http import HttpResponse
 
+from .forms import UsuarioForm, tipoForm
 from .models import Usuario, tipoUsuario
 # Create your views here.
 
@@ -13,3 +14,15 @@ def homepage(request):
     document = template.render(context)
     return HttpResponse(document)
 
+def bandanas(request):
+    bandana = open("D:/Fernanda/Documents/mk3/newPage/templates/bandana.html")
+    template = Template(bandana.read())
+    bandana.close()
+    context = Context()
+    document = template.render(context)
+    return HttpResponse(document)
+
+def crud(request):
+    usuarios = Usuario.objects.all()
+    context = {"usuario" : UsuarioForm}
+    return render(request, 'templates/user_list.html', context)
